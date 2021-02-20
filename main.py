278b062e04b0823c12117c8dff755a7263e9a2cc
joe <joseph.edwards@itv.com>
2021-02-20 14:40:52 +0000

    extremely lite

--- main.py
+++ main.py
@@ -7,24 +7,7 @@ from os.path import dirname, join
 
 
 class TestApp(MDApp):
-    dir_get_counter = 0
-
-    kivy_dir = os.getcwd()
-
-    def build(self):
-        print('build')
-        self.kivy_dir = os.getcwd() if len(os.listdir(getattr(self, 'user_data_dir'))) == 0 else self.user_data_dir
-
-    def dir_get(self, *args):
-        '''Provides a way for the .kv to load files.'''
-        path_list = [self.kivy_dir]
-        print(path_list)
-        for arg in args:
-            path_list.append(arg)
-        self.dir_get_counter += 1
-        return os.path.join(*path_list)
-
-
+    pass
 
 if __name__ == '__main__':
     TestApp().run()
